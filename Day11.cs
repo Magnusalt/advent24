fab0c729892a34d1b4e49a3418cfaafc611ce026
Magnus Altgaard <magnus.altgard@biltema.com>
2024-12-11 14:19:58 +0100

    day 11 placeholder

---
+++ Day11.cs
@@ -0,0 +1,16 @@
+public class Day11() : Day(11)
+{
+    public override long RunPart1()
+    {
+        var stones = Input;
+
+        
+
+        return 0;
+    }
+
+    public override long RunPart2()
+    {
+        return 0;
+    }
+}
